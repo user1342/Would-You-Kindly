8072d49dc8e763af24799c2e11a46846ca6a3410
James Stevenson <28961606+user1342@users.noreply.github.com>
2024-09-06 07:48:43 +0100

    formatting

--- example.py
+++ example.py
@@ -2,5 +2,10 @@ from WYK.Judge import Judge
 
 if __name__ == "__main__":
     llm_name = "unsloth/mistral-7b-instruct-v0.3-bnb-4bit"
-    judge = Judge(llm_name, llm_name, llm_name, debug=True)
+    judge = Judge(
+        red_agent_model=llm_name,
+        blue_agent_model=llm_name,
+        judge_model=llm_name,
+        debug=True,
+    )
     print(f"LLM '{llm_name}' scored {judge.assess()[0]}")
